ed7a8d443166b4f5c372223deecc064252cf1f80
Umar <abuqodir0107@gmail.com>
2024-10-04 22:57:08 +0500

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import "./assets/scss/main.scss"
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.querySelector('.wrapper')).render(
